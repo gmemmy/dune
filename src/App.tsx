3f1455b3be61ad489ec7e9c93e901ea02deb1956
Emmanuel Atawodi <gmemmy22@gmail.com>
2025-11-03 16:49:29 +0100

    chore: setup project scaffold

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+
+function App() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'red' }}>
+      <Text style={{ color: 'white' }}>Hello, World!</Text>
+    </View>
+  );
+}
+
+export default App;
